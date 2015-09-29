56d66ca601647743c2887f19e18fe48e6f17952b
jfransen44 <JFransen44@Jeremy-Fransens-MacBook-Pro.local>
2015-09-28 21:19:41 -0700

    updating main class

--- src/Assignment4.java
+++ src/Assignment4.java
@@ -9,7 +9,7 @@
 public class Assignment4
 {
 
- public static void main(String[] args)
+   public static void main(String[] args)
    {
       
       // BarcodeImage test
@@ -93,4 +93,4 @@ public class Assignment4
       //System.out.println();
    }
 
-}
+}//END main
